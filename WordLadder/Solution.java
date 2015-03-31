bb44399bb60089ad3680bb2753f1f38286a0680d
zhaohuizh <bravezhaohui@gmail.com>
2015-03-30 22:20:08 -0700

    correct Solution for Word Ladder

--- WordLadder/Solution.java
+++ WordLadder/Solution.java
@@ -2,21 +2,42 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class Solution{
-  public int ladderLength(String start, String end, Set<String> dict){
+  public static int ladderLength(String start, String end, Set<String> dict){
     Set<String> begin = new HashSet<String>();
     int depth = 1;
+    if(start.equals(end)){
+      return depth;
+    }
     int len = start.length();
     begin.add(start);
-    while(begin.isEmpty()){
+    while(!begin.isEmpty()){
+      depth++;
+      Set<String> newLevel = new HashSet<String>();
       for(String str : begin){
         for(int i = 0; i < len; i++){
-          for(int j = 0; j < 26; j++){
-            char c = 'a' + j;
-            String newStr = str.substring(0, i) + c + str.substring(i + 1);
+          for(char j = 'a'; j <= 'z'; j++){
+            String newStr = str.substring(0, i) + j + str.substring(i + 1);
+            if(newStr.equals(end)){
+              return depth;
+            }
+            if(dict.contains(newStr)){
+              newLevel.add(newStr);
+              dict.remove(newStr);
+            }
           }
         }
-        
       }
+      begin = newLevel;
     }
+    return 0;
+  }
+
+  public static void main(String[] args){
+    Set<String> dict = new HashSet<String>();
+    dict.add("a");
+    dict.add("b");
+    dict.add("c");
+    int depth = ladderLength("a", "c", dict);
+    System.out.println(depth);
   }
 }
