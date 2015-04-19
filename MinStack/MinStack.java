2ed9aefc6ef26414bf1d934908dac11839cdfc14
zhaohuizh <bravezhaohui@gmail.com>
2015-04-18 22:12:02 -0700

    min stack

--- MinStack/MinStack.java
+++ MinStack/MinStack.java
@@ -1,15 +1,28 @@
 import java.util.*;
 
 public class MinStack{
+  Stack<Integer> stack = new Stack<Integer>();
+  Stack<Integer> minStack = new Stack<Integer>();
+
   public void push(int x){
+    if(minStack.isEmpty() || x <= getMin()){
+      minStack.push(x);
+    }
+    stack.push(x);
   }
 
   public void pop(){
+    if(getMin() == top()){
+      minStack.pop();
+    }
+    stack.pop();
   }
 
   public int top(){
+    return stack.peek();
   }
 
   public int getMin(){
+    return minStack.peek();
   }
 }
