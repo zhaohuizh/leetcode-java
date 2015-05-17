03cf05c5b080ddd326acd7bfa415b3e1f77b6ebc
zhaohuizh <bravezhaohui@gmail.com>
2015-05-17 14:45:13 -0700

    refactor code

--- ZigzagConversion/Solution.java
+++ ZigzagConversion/Solution.java
@@ -4,21 +4,15 @@ public class Solution {
     if(len <= numRows || numRows == 1) return s;
     int zigSize = numRows * 2 - 2;
     StringBuilder sb = new StringBuilder();
-    for(int j = 0; j < len; j = j + zigSize){
-      sb.append(s.charAt(j));
-    }   
-    for(int i = 1; i < numRows - 1; i++){
+    for(int i = 0; i < numRows; i++){
       for(int j = i; j < len; j = j + zigSize){
         sb.append(s.charAt(j));
         int nextIdx = j + zigSize - 2 * i;
-        if(nextIdx < len){
+        if(nextIdx < len && nextIdx != j && nextIdx != j + zigSize){
           sb.append(s.charAt(nextIdx));
         }   
       }   
-    }   
-    for(int j = numRows - 1; j < len; j = j + zigSize){
-      sb.append(s.charAt(j));
-    }   
+    }
     return sb.toString();
   }
 }
