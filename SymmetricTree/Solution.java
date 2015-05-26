824d7bcd41263e8326c3b6098dbc0f97ecbb692a
Zhaohui <zhaohuz@amazon.com>
2015-05-26 15:20:17 -0700

    non recursive solution, incorrect

--- SymmetricTree/Solution.java
+++ SymmetricTree/Solution.java
@@ -22,4 +22,44 @@ public class Solution {
     return isSymmetricNode(one.left, two.right) &&
       isSymmetricNode(one.right, two.left);
   }
+  
+  public boolean isSymmetric(TreeNode root){
+    if(root == null) return true;
+    List<TreeNode> list = new ArrayList<TreeNode>();
+    list.add(root);
+    int childNum = 1;
+    while(childNum > 0){
+      int low = list.size() - childNum;
+      int hig = list.size() - 1;
+      while(low <= hig){
+        if(checkNodes(list.get(low).left, list.get(hig).right) &&
+            checkNodes(list.get(low).right, list.get(hig).left)){
+          low++;
+          hig--;
+        }else{
+          return false;
+        }
+      }
+      int i = list.size() - childNum;
+      childNum = 0;
+      for(; i < list.size() - 1; i++){
+        if(list.get(i).left != null){
+          list.add(list.get(i).left);
+          childNum++;
+        }
+        if(list.get(i).right != null){
+          list.add(list.get(i).right);
+          childNum++;
+        }
+      }
+    }
+    return true;
+  }
+
+  private boolean checkNodes(TreeNode one, TreeNode two){
+    if(one == null && two == null) return true;
+    if(one == null || two == null) return false;
+    if(one.val != two.val) return false;
+    return true;
+  }
 }
