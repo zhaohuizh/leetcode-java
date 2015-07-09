bffdedb9155c5b427d7c4ea8cb3610a72dd6e96e
zhaohuizh <bravezhaohui@gmail.com>
2015-07-08 21:34:36 -0700

    divide two integers

--- DivideTwoIntegers/Solution.java
+++ DivideTwoIntegers/Solution.java
@@ -3,18 +3,31 @@ public class Solution {
     if(divisor == 0){
       return Integer.MAX_VALUE;
     }
+    if(divisor==-1 && dividend == Integer.MIN_VALUE){
+      return Integer.MAX_VALUE;
+    }
     boolean negative = dividend < 0;
     negative ^= divisor < 0;
-    dividend = Math.abs(dividend);
-    divisor = Math.abs(divisor);
+    long dividendLong = Math.abs((long)dividend);
+    long divisorLong = Math.abs((long)divisor);
     int ret = 0;
-    while(dividend > 0){
-      dividend -= divisor;
-      ret++;
+    while(dividendLong > divisorLong){
+      int shif = 0;
+      while(dividendLong > (divisorLong << shif)){
+        shif++;
+      }
+      ret += 1 << (shif - 1);
+      dividendLong -= divisorLong << (shif - 1);
     }
     if(negative){
       ret = 0 - ret;
     }
     return ret;
+  }
+
+  public static void main(String[] args){
+    boolean a = -1 < 0;
+    a ^= -2 < 0;
+    System.out.println(a);
   }
 }
