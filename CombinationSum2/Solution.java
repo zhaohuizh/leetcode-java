240cc5dbc5c4a0b7fb646219fdfd6e256d2da982
zhaohuizh <bravezhaohui@gmail.com>
2015-07-10 22:39:01 +0000

    combination sum2

---
+++ CombinationSum2/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    List<List<Integer>> ret = new ArrayList<List<Integer>>();
+    List<Integer> comb = new ArrayList<Integer>();
+    helper(candidates, target, 0, comb, ret);
+    return ret;
+  }
+
+  private void helper(int[] candidates, int target, int idx,
+      List<Integer> curr, List<List<Integer>> ret){
+    if(target == 0){
+      ret.add(new ArrayList<Integer>(curr));
+    }else if(target > 0){
+      for(int i = idx; i < candidates.length; i++){
+        if(i > idx && candidates[i] == candidates[i - 1]){
+          continue;
+        }
+        curr.add(candidates[i]);
+        helper(candidates, target - candidates[i], i + 1, curr, ret);
+        curr.remove(curr.size() - 1);
+      }
+    }
+  }
+}
